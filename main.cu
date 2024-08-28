87998f2ecfed2921efb431a5ab254d9118a1fa3d
Alex Holmes <alex@sed.dev>
2024-08-28 19:33:50 -0400

    Add hello world

--- main.cu
+++ main.cu
@@ -1,6 +1,14 @@
-#include <iostream>
+#include<cstdio>
+#include<cstdlib>
+
+__global__ void print_from_gpu() {
+    printf("Hello World! from thread [%d,%d] From device\n", threadIdx.x,blockIdx.x);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    printf("Hello World from host!\n");
+    print_from_gpu<<<1, 1>>>();
+    cudaDeviceSynchronize();
+
+    return EXIT_SUCCESS;
 }
